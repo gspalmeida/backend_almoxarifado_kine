f047741fa7c85313af7187d5fdcead9530c5d993
Gustavo Sartori <gustavo.sartori@tecnospeed.com.br>
2021-08-25 02:18:44 -0300

    deploy: Test allow ssl

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,8 @@
 module.exports = {
   type: 'postgres',
   url: process.env.TYPEORM_DATABASE,
-  ssl: false,
-  entities: [
-    process.env.TYPEORM_ENTITIES
-  ],
-  migrations: [
-    process.env.TYPEORM_MIGRATIONS
-  ],
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
   cli: {
     migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
   },
