4a1030f0a55a2589aa976568b95d831c3d6701cf
Gustavo Sartori <gustavo.sartori@tecnospeed.com.br>
2021-08-25 02:45:28 -0300

    deploy: test default confs

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   type: 'postgres',
   url: process.env.TYPEORM_DATABASE,
+  ssl: false,
   entities: [process.env.TYPEORM_ENTITIES],
   migrations: [process.env.TYPEORM_MIGRATIONS],
   cli: {
